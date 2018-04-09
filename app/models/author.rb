7305933f6190ea900ffc96c04469276f1c0e82aa
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-09 19:41:47 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ActiveRecord::Base
-  validates :name, presence: true, uniqueness: true  
+  validates :name, presence: true, uniqueness: true
+  validates :phone_numberm length: {is: 10} 
 
 end
