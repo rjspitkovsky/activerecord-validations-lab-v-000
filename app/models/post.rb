7d0d7cafd2787bb776212e7c886a42d3dc438a5a
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-04-09 19:54:06 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
-  validates :category, inclusion: { in: %w(fiction non-fiction)}
-  validates :title, inclusion: { in: %w(won't believe secret top guess)}
+  # validates :category, inclusion: { in: %w(fiction non-fiction)}
+  # validates :title, inclusion: { in: %w(won't believe secret top guess)}
 end
